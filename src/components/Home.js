2c9b5da142fbb77227c1a7103bd7f5843b02ec62
Eric Santos <e.santos@joincea.com>
2021-05-17 12:28:15 -0600

    styled our work with stock images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react'
+import React from 'react'
 import '../styles/home.css'
 
 const Home = () => {
--- src/components/OurWork.js
+++ src/components/OurWork.js
@@ -4,12 +4,43 @@ import '../styles/our-work.css'
 const OurWork = () => {
   return (
     <div className="work">
-      <section>
-        <h1>Houston</h1>
-        <h1>Houston</h1>
-        <h1>Houston</h1>
-        <h1>Houston</h1>
-      </section>
+      <section className="title">Our Work</section>
+      <figure>
+        <blockquote cite="https://www.huxley.net/bnw/four.html">
+          <p>
+            Solar energy is the cheapest source of energy in the world as of
+            2017.
+          </p>
+        </blockquote>
+      </figure>
+
+      <div className="images">
+        <picture>
+          <img
+            srcSet="./photos/ferris-wheel.jpg 1440w"
+            sizes="(max-width: 600px) 1440px"
+            src="./photos/ferris-wheel.jpg"
+            alt="third img"
+          />
+        </picture>
+
+        <picture>
+          <img
+            srcSet="./photos/ferris-wheel.jpg 1440w"
+            sizes="(max-width: 600px) 1440px"
+            src="./photos/ferris-wheel.jpg"
+            alt="third img"
+          />
+        </picture>
+        <picture>
+          <img
+            srcSet="./photos/ferris-wheel.jpg 1440w"
+            sizes="(max-width: 600px) 1440px"
+            src="./photos/ferris-wheel.jpg"
+            alt="third img"
+          />
+        </picture>
+      </div>
     </div>
   )
 }
