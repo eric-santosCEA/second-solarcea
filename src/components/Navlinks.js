920affbd58ce417551ecf865218fc9aa034a9a92
Eric Santos <e.santos@joincea.com>
2021-06-01 11:24:12 -0600

    fixed navbar off center

--- src/components/Navlinks.js
+++ src/components/Navlinks.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { BiMenuAltRight } from 'react-icons/bi'
+import { AiOutlineMenu } from 'react-icons/ai'
+import { motion } from 'framer-motion'
 import '../styles/navlinks.css'
+
+const variants = {
+  open: { opacity: 1, y: 0 },
+  closed: { opacity: 0, y: '-100%' },
+}
 
 const Navlinks = () => {
   const [active, setActive] = useState(false)
@@ -10,37 +16,46 @@ const Navlinks = () => {
   }
   return (
     <>
-      <nav>
-        <Link to="/">
-          <img src="./photos/solar-long-logo50.png" alt="logo" height="50" />
+      <Link to="/">
+        <motion.img
+          src="./photos/solar-long-logo50.png"
+          height="50"
+          alt="logo"
+          className="logo"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        />
+      </Link>
+      <motion.div
+        className={active ? 'open' : 'navlinks'}
+        animate={active ? 'open' : 'closed'}
+        variants={variants}
+      >
+        <Link to="/" className="navlink">
+          <h1>Home</h1>
         </Link>
-        <div className={active ? 'links open' : 'links'}>
-          <Link to="/" className="link">
-            <h1>Home</h1>
-          </Link>
-          <Link to="/sales-partner" className="link">
-            <h1>Sales Partner</h1>
-          </Link>
-          <Link to="/solar-basics" className="link">
-            <h1>Solar Basics</h1>
-          </Link>
-          <Link to="/work" className="link">
-            <h1>Our Work</h1>
-          </Link>
+        <Link to="/solar-basics" className="navlink">
+          <h1>Solar Basics</h1>
+        </Link>
+        <Link to="/sales-partner" className="navlink">
+          <h1>Sales Partner</h1>
+        </Link>
+        <Link to="/work" className="navlink">
+          <h1>Our Work</h1>
+        </Link>
 
-          <a
-            href="https://ceadash.com/application/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link"
-          >
-            <h1>Careers</h1>
-          </a>
-        </div>
-        <div className="icon">
-          <BiMenuAltRight onClick={handleClick} size="3rem" />
-        </div>
-      </nav>
+        <a
+          href="https://ceadash.com/application/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="navlink"
+        >
+          <h1>Careers</h1>
+        </a>
+      </motion.div>
+      <div className="nav-icon">
+        <AiOutlineMenu size="2.5rem" onClick={handleClick} />
+      </div>
     </>
   )
 }
