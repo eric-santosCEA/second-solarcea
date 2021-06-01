5b8ba53453d77fd1a5e09c40b035d7355053dfca
Eric Santos <e.santos@joincea.com>
2021-06-01 11:42:18 -0600

    fixed footer height

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,20 +15,61 @@ const Footer = () => {
         <h1>Contact</h1>
         <div className="addresses">
           <address>
-            <h2>EL Paso, TX</h2>
-            <h3>6600 N Mesa St STE 502, El Paso, TX 79912</h3>
-            <a href="mailto:jim@rock.com">
-              <h4>Email: support@joincea.com</h4>
+            <h2>Texas HQ Office</h2>
+            <h4>
+              6600 N Mesa St STE 502,
+              <br /> El Paso, TX 79912
+            </h4>
+            <h4>Phone: (915) 201-2631</h4>
+            <a href="mailto:support@joincea.com">
+              <h4>support@joincea.com</h4>
             </a>
-            <a href="tel:+13115552368">Phone: (311) 555-2368</a>
+            <a href="tel:+13115552368">(311) 555-2368</a>
           </address>
           <address>
             <h2>Houston, TX</h2>
-            <h3>13105 Northwest Freeway, STE 1260 Houston, TX 77040</h3>
-            <a href="mailto:jim@rock.com">
-              <h4>Email: support@joincea.com</h4>
+            <h4>
+              13105 Northwest Freeway,
+              <br /> STE 1260 Houston, TX 77040
+            </h4>
+            <h4>Phone: (915) 201-2631</h4>
+            <a href="mailto:support@joincea.com">
+              <h4>support@joincea.com</h4>
             </a>
-            <a href="tel:+13115552368">Phone: (311) 555-2368</a>
+            <a href="tel:+13115552368">(311) 555-2368</a>
+          </address>
+          <address>
+            <h2>Arizona HQ Office</h2>
+            <h4>56th PI, Scottsdale, AZ 85257</h4>
+            <h4>Phone: (915) 201-2631</h4>
+            <a href="mailto:support@joincea.com">
+              <h4>support@joincea.com</h4>
+            </a>
+            <a href="tel:+13115552368">(311) 555-2368</a>
+          </address>
+          <address>
+            <h2>Florida HQ Office</h2>
+            <h4>
+              12221 N. US-301
+              <br /> Thonotosassa, FL 33592
+            </h4>
+            <h4>Phone: (915) 201-2631</h4>
+            <a href="mailto:support@joincea.com">
+              <h4>support@joincea.com</h4>
+            </a>
+            <a href="tel:+13115552368">(311) 555-2368</a>
+          </address>
+          <address>
+            <h2>Nevada HQ Office</h2>
+            <h4>
+              970 W 190th, STE 302
+              <br /> Torence, CA 90502
+            </h4>
+            <h4>Phone: (915) 201-2631</h4>
+            <a href="mailto:support@joincea.com">
+              <h4>support@joincea.com</h4>
+            </a>
+            <a href="tel:+13115552368">(311) 555-2368</a>
           </address>
         </div>
         <div className="follow-us">
